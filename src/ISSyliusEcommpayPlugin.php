b37f04397a7b6e6de00c504522dc5373b5b7d0e5
dmitry <dch@inbox.lv>
2018-10-11 16:19:48 +0300

    init

---
+++ src/ISSyliusEcommpayPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IS\SyliusEcommpayPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class ISSyliusEcommpayPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
